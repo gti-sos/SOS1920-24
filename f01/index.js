c05874cfe4b1e26b9073e4cc08c9f31f746a171b
Nerk1 <nerk1toran@gmail.com>
2020-03-01 20:18:34 +0000

    f01-server-done

--- f01/index.js
+++ f01/index.js
@@ -1,4 +1,10 @@
-console.log("Server Ready");
+
 const express = require("express");
 var app = express();
-app.get("/time", (request, response) => {response.send(<html>+express()</html>)});
+
+app.get("/time", (req,res)=>{
+	let d = new Date();
+	res.send("<html>"+d+"</html>");
+});
+app.listen(3343)
+console.log("Server Ready");
