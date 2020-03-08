fe5aa947df85931ef37a528d06a53e1076d05afd
vicpaltor <victormpalostorres@gmial.com>
2020-03-08 15:25:07 +0000

    Por grupo: 12/03/2020

--- f01/index.js
+++ f01/index.js
@@ -2,9 +2,15 @@
 const express = require("express");
 var app = express();
 
+app.use("/",express.static("./public"));
+
 app.get("/time", (req,res)=>{
 	let d = new Date();
 	res.send("<html>"+d+"</html>");
 });
-app.listen(3343)
-console.log("Server Ready");
+
+app.listen(3005, ()=>{
+	console.log("Server Ready");
+});
+
+console.log("Starting server...");
