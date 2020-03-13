ae17e5dc025aa311b0a4e262690bd3b93555343a
Nerk1 <nerk1toran@gmail.com>
2020-03-13 11:37:27 +0000

    l06toran

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
 const express = require("express");
 var app = express();
+const bodyParser = require("body-parser")
 
 var port = process.env.PORT || 80;
 
+app.use(bodyParser.json());
 
 app.use("/",express.static("./public"));
+var contacts = [{
+	name: "peter",
+	phone: 123456
+},
+{name: "peter2",
+	phone: 1234567
+}];
+const BASE_API_URL= "/api/v1";
+app.get(BASE_API_URL+"/contacts", (req,res)=>{
+	res.send(JSON.stringify(contacts,null,2));
+});
+app.post(BASE_API_URL+"/contacts",(req,res)=>{
+	contacts.push(req.body);
+	res.sendStatus(201,"CREATED");
+} );
 
 app.get("/time", (req,res)=>{
 	let d = new Date();
