4bc56203626f744244a95dcfcc698d6d9924aec8
Nerk1 <nerk1toran@gmail.com>
2020-03-20 11:15:38 +0000

    l07

--- index.js
+++ index.js
@@ -15,14 +15,79 @@ var contacts = [{
 {name: "peter2",
 	phone: 1234567
 }];
+
 const BASE_API_URL= "/api/v1";
+//GET CONTACTS
 app.get(BASE_API_URL+"/contacts", (req,res)=>{
 	res.send(JSON.stringify(contacts,null,2));
 });
+//POST CONTACTS
 app.post(BASE_API_URL+"/contacts",(req,res)=>{
-	contacts.push(req.body);
-	res.sendStatus(201,"CREATED");
+	var newContact=req.body;
+	if((newContact=="") || (newContact.name==null)){
+		res.sendStatus(400,"BAD REQUIEST(no name provided)");
+		
+	}else{
+		contacts.push(newContact);
+		res.sendStatus(201,"CREATED");
+	}
 } );
+
+//DELETE CONTACTS
+app.delete(BASE_API_URL+"/contacts", (req,res)=>{
+	res.sendStatus(400,"DELETED req CONTACT");
+});
+
+//GET CONTACT/XXX //DONE
+app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	var name = req.params.name;
+	var filteredContacts = contacts.filter((c)=>{
+		return (c.name == name);
+	});
+	if(filteredContacts.length>=1){
+		res.send(filteredContacts[0]);
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND")
+	}
+});
+
+//POST CONTACT/XXX
+app.post(BASE_API_URL+"/contacts/:name",(req,res)=>{
+	var newContact=req.body;
+	if((newContact=="") || (newContact.name==null)){
+		res.sendStatus(400,"BAD REQUIEST(no name provided)");
+		
+	}else{
+		contacts.push(newContact);
+		res.sendStatus(201,"CREATED");
+	}
+} );
+
+//PUT CONTACT/XXX
+app.put(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	var name = req.params.name;
+	res.send(JSON.stringify(name,null,2));
+});
+
+//DELETE CONTACT/XXX
+
+app.get(BASE_API_URL+"/contacts/:name", (req,res)=>{
+	var name = req.params.name;
+	var filteredContacts = contacts.filter((c)=>{
+		return (c.name != name);
+	});
+	if(filteredContacts.length < contacts.length){
+		res.sendStatus(200,"CONTACT DELETED");
+		contacts = filteredContacts;
+	}else{
+		res.sendStatus(404,"CONTACT NOT FOUND FOR DELETE");
+	}
+	
+});
+
+
+
+
 
 app.get("/time", (req,res)=>{
 	let d = new Date();
