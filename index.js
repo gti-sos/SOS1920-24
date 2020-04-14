347b00d6f07a1366b2bb8282daef0eebd812bb4c
Nerk1 <nerk1toran@gmail.com>
2020-04-14 20:00:43 +0000

    resolviendoFails

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express"); //INCLUDING EXPRESS MODULE ON A CONST
 const bodyParser = require("body-parser"); //INCLUDING BODY-PARSER
 const path = require("path");
 
+const atcAPI = require(path.join(__dirname ,"atcAPI"));
 const intcontAPI = require(path.join(__dirname, "intcontAPI"));
 
 const port = process.env.PORT || 80; //HTTP CONNECTION MANAGING
@@ -12,7 +13,8 @@ const app = express(); //CALLING EXPRESS
 app.use(bodyParser.json());
 app.use("/",express.static("./public")); //ROOT DIRECTORY
 
-intcontAPI(app); //puedo importar las apis que yo quiera
+atcAPI(app);
+intcontAPI(app);
 
 app.listen(port, ()=>{
 	console.log("Server Ready");
