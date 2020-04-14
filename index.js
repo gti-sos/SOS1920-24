21df7a82d75e496b49ea4ef04924b11d6b33a028
Nerk1 <nerk1toran@gmail.com>
2020-04-14 19:46:14 +0000

    enmismue

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 //SERVER MANAGING
 const express = require("express"); //INCLUDING EXPRESS MODULE ON A CONST
-const bodyParser = require("body-parser") //INCLUDING BODY-PARSER
-const path = require("path"); //para hacer que funcione en linux o en windows
-//PARA MODULARIZAR
-const atcAPI = require(path.join(__dirname ,"atcAPI"));//para usar la libreria nedb
+const bodyParser = require("body-parser"); //INCLUDING BODY-PARSER
+const path = require("path");
+
 const intcontAPI = require(path.join(__dirname, "intcontAPI"));
 
 const port = process.env.PORT || 80; //HTTP CONNECTION MANAGING
+
 const app = express(); //CALLING EXPRESS
 
-//USING BODYPARSER AND DECLARING ROOT DIRECTORY
 app.use(bodyParser.json());
-app.use("/",express.static("./public"));
+app.use("/",express.static("./public")); //ROOT DIRECTORY
 
-//API VICTOR para modulos separados
-atcAPI(app);
+intcontAPI(app); //puedo importar las apis que yo quiera
 
-
-app.listen(port, () => {
-	console.log("Server ready");
+app.listen(port, ()=>{
+	console.log("Server Ready");
 });
-
 console.log("Starting server...");
+
