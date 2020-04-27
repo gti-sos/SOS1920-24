c5fcbaa9cad11929a9b73b459d961c4132cac0b2
vicpaltor <victormpalostorres@gmail.com>
2020-04-27 21:04:31 +0200

    falta que funciones los botones

--- src/front/ContactsTable.svelte
+++ src/front/ContactsTable.svelte
@@ -1,6 +1,23 @@
 <script>
+	//importamos la FUNCION onMount
+	import {onMount} from "svelte";
+	//usamos el GUI para tablas
+	import Table from "sveltestrap/src/Table.svelte";
+	//usamos el GUI para botones
+	import Button from "sveltestrap/src/Button.svelte";
+	
+
 	//cargar datos desde la API 
 	let atc = [];
+	let newAtc = {
+		aut_com: "",
+		year:    "",
+		espce:   "",
+		yaq:     "",
+		obu:    ""
+	};
+
+	onMount(getAtc);
 	
 	async function getAtc(){
 		console.log("Fetching atc");
@@ -19,48 +36,65 @@
 		}
 	}
 
-	getAtc();
+	async function insertAtc(){
 
+		console.log("Inserting atc"+JSON.stringify(newAtc));
+		//fetch es la solicitud a la API
+		const res = await fetch("/api/v1/atc-stats", {
+			method: "POST",
+			body: JSON.stringify(newAtc),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+
+	}
+
+
+	
 </script>
 
 <main>
-	<!--quiero que esperes a tener todos los datos, sino aparece 0 y luego se cargan -->
-	{#await atc}
-	    Datos: {atc.length}
+	{#await atc} 
+
+	{:then atc}
+			<Table bordered> 
+				<thead>
+					<tr>
+						<th>aut_com </th>
+						<th>year  </th>
+						<th>espce </th>
+						<th>yaq   </th>
+						<th>obu   </th>	
+						<th>Actions </th>	
+					</tr>
+				</thead>
+				<tbody>
+					
+					<tr> 
+						<td><input bind:value = "{newAtc.aut_com}"> </td>
+						<td><input bind:value = "{newAtc.year}">    </td>
+						<td><input bind:value = "{newAtc.espce}">   </td>
+						<td><input bind:value = "{newAtc.yaq}">     </td>
+						<td><input bind:value = "{newAtc.obu}">     </td>
+						<td> <Button outline color="primary"  on:click={insertAtc} 
+							> Insert </Button> </td>
+					</tr>
+					
+					
+					<!--para iterar con svelte-->
+					{#each atc as e}
+					<tr> 
+						<td>{e.aut_com} </td>
+						<td>{e.year}  </td>
+						<td>{e.espce} </td>
+						<td>{e.yaq}   </td>
+						<td>{e.obu}   </td>
+						<td> <Button outline color="danger"> Delete </Button> </td>
+					</tr>
+					{/each}
+				</tbody>
+			</Table>
 	{/await}
-
-
-	<table> 
-		<thead>
-			<tr>
-				<td>ID </td>
-				<td>aut_com </td>
-				<td>year  </td>
-				<td>espce </td>
-				<td>yaq   </td>
-				<td>obu   </td>	
-			</tr>
-		</thead>
-		<tbody>
-			<tr> 
-				<td>#####</td>
-				<td>Andalucia </td>
-				<td>2018  </td>
-				<td>757.2 </td>
-				<td>757   </td>
-				<td>757   </td>
-			</tr>
-		</tbody>
-
-	   	
-
-	</table>
-	
 	
 </main>
-
-<style>
-	table {
-		border: 1px solid black;
-	}
-</style>
